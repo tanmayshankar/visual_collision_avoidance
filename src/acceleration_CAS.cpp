de822640d847bc0cb9ac7d04bd3889309fadf76b
ermueller2000 <emueller@gmail.com>
2016-02-04 11:01:39 -0800

    Update acceleration_CAS.cpp

--- src/acceleration_CAS.cpp
+++ src/acceleration_CAS.cpp
@@ -451,8 +451,8 @@ int main(int argc, char **argv)
       own_pose_sub = nh_.subscribe("mavros/position/local",1,gps_own_pose_callback);
       own_vel_sub = nh_.subscribe("mavros/global_position/gps_vel",1,gps_own_vel_callback);
       // intruder_pose_sub = nh_.subscribe("odroid1/mavros/local_position/local",1,gps_intruder_pose_callback);
-      intruder_pose_sub = nh_.subscribe("odroid1/mavros/position/local",1,gps_intruder_pose_callback);
-      intruder_vel_sub = nh_.subscribe("odroid1/mavros/global_position/gps_vel",1,gps_intruder_vel_callback);
+      intruder_pose_sub = nh_.subscribe("odroid_1/mavros/position/local",1,gps_intruder_pose_callback);
+      intruder_vel_sub = nh_.subscribe("odroid_1/mavros/global_position/gps_vel",1,gps_intruder_vel_callback);
 
       // set_pt_vel_pub = nh_.advertise<geometry_msgs::TwistStamped>("/mavros/cmd_vel",1);    
       // set_pt_vel_pub_1 = nh_.advertise<geometry_msgs::TwistStamped>("/mavros/setpoint_velocity/cmd_vel",1);  
